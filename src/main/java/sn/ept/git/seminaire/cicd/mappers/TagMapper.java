d588c82ac03f0b73965e8a1d79ef63bd3c184977
senei <senei@senei.sn>
2025-05-20 14:09:37 -0400

    creat secret scan action

--- src/main/java/sn/ept/git/seminaire/cicd/mappers/TagMapper.java
+++ src/main/java/sn/ept/git/seminaire/cicd/mappers/TagMapper.java
@@ -4,7 +4,28 @@ import sn.ept.git.seminaire.cicd.models.TagDTO;
 import sn.ept.git.seminaire.cicd.entities.Tag;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+import java.util.Optional;
+
 @Mapper(componentModel = "spring")
-public   interface TagMapper extends GenericMapper<Tag, TagDTO> {
+public   interface TagMapper {
+
+    Tag toEntity(TagDTO dto);
+
+    TagDTO toDTO(Tag entity);
+
+    default List<Tag> toEntitiesList(List<TagDTO> dtoList){
+        return Optional.ofNullable(dtoList)
+                .orElse(List.of())
+                .stream()
+                .map(this::toEntity)
+                .toList();
+    }
+
+    default List<TagDTO> toDTOlist(List<Tag> entitiesList){
+        return Optional.ofNullable(entitiesList)
+                .orElse(List.of())
+                .stream().map(this::toDTO).toList();
+    }
 
 }
